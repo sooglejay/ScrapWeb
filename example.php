78a12fa495fd4ca84e2e3abf635c3d8b8876a943
jianwei <jianwei@ali-985aebd5fed8.local>
2017-07-15 11:24:36 +0800

    installation

--- example.php
+++ example.php
@@ -10,7 +10,7 @@ $host = 'http://localhost:4444/wd/hub'; // this is the default
 $capabilities = DesiredCapabilities::chrome();
 $driver = RemoteWebDriver::create($host, $capabilities, 5000);
 // navigate to 'http://www.baidu.com/'
-$driver->get('http://www.baidu.com/');
+$driver->get('http://www.63si.com.cn:8888/lswtqt/toLogin.jhtml');
 // adding cookie
 $driver->manage()->deleteAllCookies();
 $cookie = new Cookie('cookie_name', 'cookie_value');
@@ -22,7 +22,26 @@ print_r($cookies);
 echo "The title is '" . $driver->getTitle() . "'\n";
 // print the URI of the current page
 echo "The current URI is '" . $driver->getCurrentURL() . "'\n";
+$isLogin = false;
+$userNameElem = null;
+$passwordElem = null;
+try{
+    $userNameElem = $driver->findElement(WebDriverBy::id('username'));
+    $passwordElem = $driver->findElement(WebDriverBy::id('password'));
+}catch (\Exception $e){
+    $isLogin = true;
+}
+finally{
+    if($isLogin){
+        echo "Logined!";
+    }else{
+        $headerText = $userNameElem->getText();
+        $passwordText = $passwordElem->getText();
+        echo "No Logined!--".$headerText." -- ".$passwordText;
+    }
+}
+// $element will be instance of RemoteWebElement
 
 // submit the form
 // close the Firefox
-$driver->quit();
+//$driver->quit();
--- readme.txt
+++ readme.txt
@@ -1,3 +1,14 @@
 This is a php scrap demo using php-webdriver powered by facebook
 
+1. Download
+
+ChromeWebDriver:https://sites.google.com/a/chromium.org/chromedriver/downloads
+
+SeleniumServer:selenium-server-standalone-3.4.0.jar that I have downloaded for you below install folder
+
+
+
+2. Install Selenium ：
+
+java  -Dwebdriver.chrome.driver=/your path to webdriver/chromedriver -jar  ./install/selenium-server-standalone-3.4.0.jar
 
